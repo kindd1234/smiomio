4e1df2d3dd8592c7984beb3d3852385316ce8350
kindd1234 <255912237+kindd1234@users.noreply.github.com>
2026-02-11 15:23:07 +0100

    Create 2026_02_11_152211_add_admin.php

---
+++ database/migrations/2026_02_11_152211_add_admin.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table(
+            'users'
+        )->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => password_hash('admin', PASSWORD_DEFAULT),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
